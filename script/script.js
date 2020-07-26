62007d46eab51fec4544560aec3217071e299181
Krupali Thakkar <krupalitp@outlook.com>
2020-07-26 14:10:41 -0400

    modified index file and JS file

--- script/script.js
+++ script/script.js
@@ -18,20 +18,21 @@ zodiacApp.questions = [
 
 //define init method
 zodiacApp.init = () => {
-    zodiacApp.questions.forEach((question) => {
-        console.log(question);
-    
+//define init method
+zodiacApp.questions.forEach((question) => {
+//console.log(question);
+
 //Event listener -> change in my radio inputs    
     $(`input[name="${question.label}"]`).on('change', function() {
             
 //determine the input that has been selected    
-    const userChoice = $(`input[name="${question.label}"]:checked`).val();
+    const $userChoice = $(`input[name="${question.label}"]:checked`).val();
 //display message on based on selection    
-    if (userChoice === question.answer) {
+    if ($userChoice === question.answer) {
 //console.log('Getting the answer');
-    $(`.${question.label}-answer`).text(`Yaaaasss! you got it right its ${question.answer}!`)
+    $(`.${question.label}-answer`).text(` ✨ Yaaaasss! you got it right its ${question.answer}! ✨ `)
     } else {
-    $(`.${question.label}-answer`).text(`Sorry! you got it wrong`);
+    $(`.${question.label}-answer`).text(`❌ Sorry! you got it wrong ❌`);
             }
         })
     })
